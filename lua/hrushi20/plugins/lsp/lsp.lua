2309f0dc690c67c6cbfcbbf5efb81330199eb4eb
Hrushi20 <hrushikesh20thegreat@gmail.com>
2025-06-08 18:18:23 +0530

    fix lsp-zero enter on recommendation
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- lua/hrushi20/plugins/lsp/lsp.lua
+++ lua/hrushi20/plugins/lsp/lsp.lua
@@ -4,6 +4,32 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
 end)
 
+-- Manually configure nvim-cmp (required in v3)
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  },
+
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'path' },
+    { name = 'buffer' },
+    { name = 'luasnip' },
+  },
+
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
+
 -- Optional: set default preferences
 lsp_zero.set_preferences({
   suggest_lsp_servers = true,
