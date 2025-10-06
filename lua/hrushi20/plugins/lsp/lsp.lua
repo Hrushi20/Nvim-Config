a425bfe70862504ecfa688edefcf1a25f169e76d
Hrushi20 <hrushikesh20thegreat@gmail.com>
2025-10-07 00:20:11 +0530

    add tab/Shift + Tab for coc
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- lua/hrushi20/plugins/lsp/lsp.lua
+++ lua/hrushi20/plugins/lsp/lsp.lua
@@ -4,6 +4,10 @@ vim.cmd [[
   nmap <silent> gr <Plug>(coc-references)
   nmap <silent> K :call CocActionAsync('doHover')<CR>
   nmap <silent> <leader>d :TroubleToggle document_diagnostics<CR>
-  " Confirm selected completion item
-  inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
+  " Navigate completion popup menu
+  inoremap <silent><expr> <Tab>   coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
+  inoremap <silent><expr> <S-Tab> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
+
+  " Confirm selection with Enter
+  inoremap <silent><expr> <CR>    coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
 ]]
