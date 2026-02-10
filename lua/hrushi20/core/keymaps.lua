8e8a8d28740903fbb78b1309a2f4e77dad91a737
Hrushi20 <hrushikesh20thegreat@gmail.com>
2026-02-11 00:04:25 +0530

    Add resize, move lines in visual mode keymaps
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- lua/hrushi20/core/keymaps.lua
+++ lua/hrushi20/core/keymaps.lua
@@ -4,10 +4,25 @@ local keymap = vim.keymap
 
 -- general keymaps
 
+-- Buffer Keymaps
 keymap.set('n', 'H', ':bprevious<CR>', { desc = 'Previous buffer' })
 keymap.set('n', 'L', ':bnext<CR>', { desc = 'Next buffer' })
 keymap.set('n', '<leader>bb', ':Telescope buffers<CR>', { desc = 'Browse buffers' })
 keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = 'Delete buffer' })
+
+-- Resize windows
+keymap.set('n', '<C-Up>', ':resize +2<CR>', { desc = 'Increase window height' })
+keymap.set('n', '<C-Down>', ':resize -2<CR>', { desc = 'Decrease window height' })
+keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Decrease window width' })
+keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Increase window width' })
+
+-- Move lines up/down in visual mode
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
+
+-- Keep cursor centered when scrolling
+keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' })
+keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' })
 
 
 -- Use Tabs to navigate Dropdown for autocomplete
