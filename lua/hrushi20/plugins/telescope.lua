d418ff65a45971c8a4f06150b25f83cd3caf2097
Hrushi20 <hrushikesh20thegreat@gmail.com>
2026-02-15 12:12:38 +0530

    Optimize fuzzy file to git tracked files
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- lua/hrushi20/plugins/telescope.lua
+++ lua/hrushi20/plugins/telescope.lua
@@ -2,11 +2,11 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     keys = {
-      {'<leader>ff',':Telescope find_files<CR>', {desc = 'Find All Files in Project'}},
+      {'<leader>ff',':Telescope git_files<CR>', {desc = 'Find Git Tracked Files in Project'}},
       {'<leader>fg',':Telescope live_grep<CR>', {desc = 'Grep in Files'}},
       {'<leader>fb',':Telescope buffers<CR>', {desc = 'Find files in Buffer'}},
       {'<leader>fh',':Telescope help_tags<CR>', {desc = 'Help Tags'}},
-      {'<leader>gf',':Telescope git_files<CR>', {desc = 'Find Git tracked Files'}},
+      {'<leader>gf',':Telescope find_files<CR>', {desc = 'Find All Files in Project'}},
     },
     dependencies = {
       'nvim-lua/plenary.nvim', --[[ Mandatory for Telescope ]]
